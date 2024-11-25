0de84b02a43fb72f06571d62b074447f3c501741
Lauri Kodisoja <laurikodisoja@windowslive.com>
2024-11-25 15:24:38 +0200

    ripped one of my eyes out of my head, but still fixed the styling of the code

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,15 +1,19 @@
 // src/Navbar.jsx
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ onMenuItemClick }) => {
   return (
     <nav className="navbar">
-      <Link to="/" className="navbar-home-button">Home</Link>
+      <Link to="/" className="navbar-home-button">
+        Home
+      </Link>
       <div className="dropdown">
         <button className="dropdown-button">Inventory</button>
         <div className="dropdown-content">
-          <button onClick={() => onMenuItemClick('Add new item')}>Add New Item</button>
+          <button onClick={() => onMenuItemClick("Add new item")}>
+            Add New Item
+          </button>
           <Link to="/view-items">View All Items</Link>
           <Link to="/view-custom-labels">View Custom Labels</Link>
         </div>
@@ -17,14 +21,18 @@ const Navbar = ({ onMenuItemClick }) => {
       <div className="dropdown">
         <button className="dropdown-button">Locations</button>
         <div className="dropdown-content">
-          <button onClick={() => onMenuItemClick('Add new location')}>Add New Location</button>
+          <button onClick={() => onMenuItemClick("Add new location")}>
+            Add New Location
+          </button>
           <Link to="/view-locations">View All Locations</Link>
         </div>
       </div>
       <div className="dropdown">
         <button className="dropdown-button">Categories</button>
         <div className="dropdown-content">
-          <button onClick={() => onMenuItemClick('Add new category')}>Add New Category</button>
+          <button onClick={() => onMenuItemClick("Add new category")}>
+            Add New Category
+          </button>
           <Link to="/view-categories">View All Categories</Link>
         </div>
       </div>
