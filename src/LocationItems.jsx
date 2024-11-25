d05081a39518b9245abfa20da5152d059a414522
Lauri Kodisoja <laurikodisoja@windowslive.com>
2024-11-25 15:21:58 +0200

    styled code

--- src/LocationItems.jsx
+++ src/LocationItems.jsx
@@ -1,13 +1,20 @@
 // src/LocationItems.jsx
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import ListView from './ListView';
+import React from "react";
+import { useParams } from "react-router-dom";
+import ListView from "./ListView";
 
 const LocationItems = ({ items, updateItem, deleteItem }) => {
   const { locationName } = useParams();
-  const filteredItems = items.filter(item => item.location === locationName);
+  const filteredItems = items.filter((item) => item.location === locationName);
 
-  return <ListView title={`Items in ${locationName}`} items={filteredItems} updateItem={updateItem} deleteItem={deleteItem} />;
+  return (
+    <ListView
+      title={`Items in ${locationName}`}
+      items={filteredItems}
+      updateItem={updateItem}
+      deleteItem={deleteItem}
+    />
+  );
 };
 
 export default LocationItems;
