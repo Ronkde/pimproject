4b2b9058faf9cb591cf284a8fefe6cb20c19571e
Lauri Kodisoja <laurikodisoja@windowslive.com>
2024-11-25 15:23:26 +0200

    fixed some dogshit code

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,6 +1,6 @@
 // src/components/Modal.jsx
-import React from 'react';
-import './Modal.css';
+import React from "react";
+import "./Modal.css";
 
 const Modal = ({ title, children, onClose }) => {
   return (
@@ -8,11 +8,11 @@ const Modal = ({ title, children, onClose }) => {
       <div className="modal">
         <div className="modal-header">
           <h2>{title}</h2>
-          <button onClick={onClose} className="close-button">&times;</button>
+          <button onClick={onClose} className="close-button">
+            &times;
+          </button>
         </div>
-        <div className="modal-content">
-          {children}
-        </div>
+        <div className="modal-content">{children}</div>
       </div>
     </div>
   );
